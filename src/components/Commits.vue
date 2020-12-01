e64baa40d4b9a22c0421b1be11cbb6debceb2724
Jiri Smitek <smitek111@gmail.com>
2020-12-01 12:30:39 +0100

    přidání více informací u commitů

--- src/components/Commits.vue
+++ src/components/Commits.vue
@@ -2,7 +2,11 @@
   <div>
     <h2>Commits</h2>
     <div class="item" v-for="(commit, index) in getCommits" :key="index">
-      <p>{{ commit.commit.author.date }}</p>
+      <p>
+        <strong>Author: {{ commit.commit.author.name }}</strong>
+      </p>
+      <p>Date: {{ commit.commit.author.date }}</p>
+      <p>Message: {{ commit.commit.message }}</p>
     </div>
   </div>
 </template>
@@ -28,12 +32,17 @@ export default {
 .item {
   background: #999;
   margin: auto;
-  padding: 0.5rem 1rem;
+  padding: 0.7rem 1rem;
   margin: 0.3rem 0;
   border-radius: 1rem;
 }
 
 h2 {
   text-align: center;
+}
+
+p {
+  border-bottom: 1px #333 dashed;
+  margin-bottom: 0.6rem;
 }
 </style>
